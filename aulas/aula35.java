a99c521c3040dd146e6f6308d47be2d5496758b6
JhoeErick <jhoeerickifpr@gmail.com>
2024-11-05 20:32:19 -0300

    ok

--- aulas/aula35.java
+++ aulas/aula35.java
@@ -2,6 +2,7 @@ package aulas;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 import libs.Entrada;
 import libs.caractere;
 import libs.Vetores;
@@ -9,44 +10,62 @@ import libs.tempo;
 
 public class aula35 {
     public static void aula() {
-        System.out.println("Aula do dia 31/10 - Fila");
-        ex01();
-        //ex02();
-        //ex03();
-        //ex04();
+        System.out.println("Aula do dia 31/10 - Fila e Pilha");
+        // ex01();
+        ex02();
+        // ex03();
+        // ex04();
     }
 
-public static void ex01() {
-    Queue<Integer> fila = new LinkedList<>();
-    fila.add(15);
-    fila.add(20);
-    fila.add(50);
-    fila.add(-10);
-    fila.add(18);
+    public static void ex01() {
+        Queue<Integer> fila = new LinkedList<>();
+        fila.add(15);
+        fila.add(20);
+        fila.add(50);
+        fila.add(-10);
+        fila.add(18);
 
-    System.out.println(fila.isEmpty());
-    
-    int x = fila.poll();
-    System.out.println(x);
+        System.out.println(fila.isEmpty());
 
-
-    while(!fila.isEmpty()) {
-        x = fila.poll();
+        int x = fila.poll();
         System.out.println(x);
-    }
+
+        while (!fila.isEmpty()) {
+            x = fila.poll();
+            System.out.println(x);
+        }
 
     }
 
-public static void ex02() {
+    public static void ex02() {
+        Stack<Integer> pilha = new Stack<>();
+        int x;
 
-}
+        pilha.add(15);
+        pilha.add(20);
+        pilha.add(50);
+        pilha.add(-10);
+        pilha.add(18);
 
-public static void ex03() {
+        while (!pilha.isEmpty()) {
+            x = pilha.pop();
+            System.out.println(x);
+        }
 
-}
+    }
 
-public static void ex04() {
+    public static void ex03() {
 
-}
+    }
 
+    public static double calcularPolonesaReversa(String[] valores) {
+        Stack<Double> pilha = new Stack<>();
+        pilha.add(2);
+
+        
+    }
+
+    public static void ex04() {
+
+    }
 }
